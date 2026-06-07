06785e6816e69dbdafe1834bbef1bcd791f4413b
agupta362 <antrikshgupta0@gmail.com>
2026-06-06 23:14:07 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from fastapi import FastAPI
+import psycopg2
+
+app = FastAPI()
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST", "localhost"),
+    database=os.getenv("DB_NAME", "myapi"),
+    user=os.getenv("DB_USER", "postgres"),
+    password=os.getenv("DB_PASSWORD", "9866115169@Ag")
+)
+
+@app.get("/")
+def root():
+    return{"message": "Hello World"}
+
+@app.get("/users")
+def get_users():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    users = []
+    for row in rows:
+        users.append({"id": row[0], "name": row[1], "age": row[2]})
+    return {"users": users}
+
+@app.post("/users")
+def create_user(name: str, age: int):
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (name, age) VALUES (%s, %s)", (name, age))
+    conn.commit()
+    return {"message": "User created"}
+@app.put("/users/{id}")
+def update_user(id:int, name: str, age: int):
+    cursor = conn.cursor()
+    cursor.execute("Update users SET name=%s, age=%s WHERE id=%s",(name,age,id))
+    conn.commit()
+    return{"message": "User updated"}
+
+@app.delete("/users/{id}")
+def dlete_user(id:int):
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM users WHERE id=%s",(id,))
+    conn.commit()
+    return{"message": "User deleted"}
